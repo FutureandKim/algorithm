bae408b8f32cdfd57df156ba82bef77e3c45df63
김미래 <95979743+FutureandKim@users.noreply.github.com>
2026-04-26 12:27:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 35.50 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,38 +1,36 @@
 function solution(maps) {
-    
     const dx = [1, -1, 0, 0];
     const dy = [0, 0, 1, -1];
     
     const n = maps.length;
     const m = maps[0].length;
     
-    const queue = [];
-    queue.push([0, 0]);
+    const path = [];
+    path.push([0,0]);
     
-    const dist = new Array(n);
-    for(let j = 0; j < n; j++) {
-        dist[j] = new Array(m).fill(0);
+    const visited = new Array(n);
+    for(let i = 0; i < n; i++) {
+        visited[i] = new Array(m).fill(0);
     }
-    dist[0][0] = 1;
-
-    while(queue.length) {
-        const [x, y] = queue.shift();
+    visited[0][0] = 1;
+    
+    while(path.length) {
+        const [x, y] = path.shift();
         
         for(let i = 0; i < 4; i++) {
             const nx = x + dx[i];
             const ny = y + dy[i];
             
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if(nx >= n || ny >= m || nx < 0 || ny < 0) continue;
             
-            if(maps[nx][ny] === 0 || dist[nx][ny] !== 0) continue;
+            if(maps[nx][ny] === 0 || visited[nx][ny] > 0) continue;
             
-            dist[nx][ny] = dist[x][y] + 1;
+            visited[nx][ny] = visited[x][y] + 1;
             
-            if(nx === n - 1 && ny === m -1) return dist[nx][ny];
+            if(nx === n-1 && ny === m-1) return visited[nx][ny];
             
-            queue.push([nx, ny]);
+            path.push([nx, ny]);
         }
     }
-    
     return -1;
 }
